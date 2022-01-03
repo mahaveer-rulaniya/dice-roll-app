7c54021a538ba6ba8b0c6cf5657fad7012bbc018
Mahaveer Rulaniya <65475928+mahaveer-rulaniya@users.noreply.github.com>
2022-01-03 10:11:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var randomNumber1= Math.random();
+randomNumber1=Math.floor(randomNumber1*6)+1;
+
+var randomDiceNumber = "dice"+ randomNumber1+".png";
+var randomImageNumber = "images/"+ randomDiceNumber;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageNumber);
+
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomDiceNumber2 = "dice"+randomNumber2+".png";
+var randomImageNumber2="images/"+randomDiceNumber2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageNumber2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="Oops!! Draw";
+}
+
+
